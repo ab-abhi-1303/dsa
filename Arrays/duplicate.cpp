62d004b8593a7fc92bcf474369fe62c9b2ea453a
Abhishek Sharma <64771976+ab-abhi-1303@users.noreply.github.com>
2021-05-07 12:34:06 +0530

    duplicate in N + 1 integers

---
+++ Arrays/duplicate.cpp
@@ -0,0 +1,12 @@
+//question link: https://leetcode.com/problems/find-the-duplicate-number/
+
+int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 1 ; i < nums.size() ; i++) {
+            if(nums[i] == nums[i - 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
