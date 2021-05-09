3cb64c0ec0e92413d295809f37dc8a64be1ca64a
Abhishek Sharma <64771976+ab-abhi-1303@users.noreply.github.com>
2021-05-09 13:30:11 +0530

    find max product subarray in aray of +ve & -ve nos

---
+++ Arrays/maxProductSubArray.cpp
@@ -0,0 +1,26 @@
+//question link: https://practice.geeksforgeeks.org/problems/maximum-product-subarray3604/1
+
+class Solution{
+public:
+
+	// Function to find maximum product subarray
+	long long maxProduct(int *arr, int n) {
+	    // code here
+	    
+	    long long mi,ma, res;
+	    
+	    mi = ma = res = arr[0];
+	    
+	    for(int i = 1 ; i < n ; i++) {
+	        if(arr[i] < 0) {
+	            swap(mi, ma);
+	        }
+	        
+	        ma = max((long long)arr[i], ma * arr[i]);
+	        mi = min((long long)arr[i], mi * arr[i]);
+	        
+	        res = max(res, ma);
+	    }
+	    return res;
+	}
+};
